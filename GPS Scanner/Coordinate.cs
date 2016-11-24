0edf40b69898d1e26e90e7f2cc17c8e066f66316
Sean Dooher <sean.dooher@berkeley.edu>
2016-11-24 14:38:57 -0800

    Initial commit
    Basic functionality, reads, interprets and logs GPS data

---
+++ GPS Scanner/Coordinate.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO.Ports;
+
+namespace GPS_Scanner
+{
+    public class Coordinate
+    {
+        #region Enums
+        public enum CoordinateStyle
+        { DegreeDecimal, DegreeMinuteDecimal, DegreeMinuteSecond }
+        #endregion
+
+        #region Properties
+        public CoordinateComponent Latitude
+        {
+            get;
+            private set;
+        }
+        public CoordinateComponent Longitude
+        {
+            get;
+            private set;
+        }
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Constructors
+        public Coordinate(double lat, CoordinateComponent.CardinalDirection latDir, double lon, CoordinateComponent.CardinalDirection longDir):
+            this(new CoordinateComponent(lat,latDir),new CoordinateComponent(lon,longDir)) { }
+
+        public Coordinate(double lat, double lon):
+            this(new CoordinateComponent(lat,CoordinateComponent.ComponentType.Latitude), new CoordinateComponent(lon, CoordinateComponent.ComponentType.Longitude)) { }
+
+        public Coordinate(CoordinateComponent lat, CoordinateComponent lon)
+        {
+            Latitude = lat;
+            Longitude = lon;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Retreives a GPS Coordinate from the specified GPS
+        /// </summary>
+        /// <param name="GPS">SerialPort that represents GPS</param>
+        public static Coordinate GetCoordinates(SerialPort GPS)
+        {
+            if (!GPS.IsOpen)
+                GPS.Open();
+            string gpsReadout = String.Empty;
+            while (!gpsReadout.StartsWith("$GPGGA") && !gpsReadout.StartsWith("$GPRMC"))
+            {
+                gpsReadout = GPS.ReadLine();
+            }
+            GPS.Close();
+
+            double latitude;
+            CoordinateComponent.CardinalDirection longDir;
+            double longitude;
+            CoordinateComponent.CardinalDirection latDir;
+            string[] readout = gpsReadout.Split(',');
+
+            try
+            {
+                if (gpsReadout.StartsWith("$GPGGA"))
+                {
+                    latitude = CoordinateComponent.DegreeMinutesToDecimal(double.Parse(readout[2]));
+                    latDir = (CoordinateComponent.CardinalDirection)readout[3][0];
+                    longitude = CoordinateComponent.DegreeMinutesToDecimal(double.Parse(readout[4]));
+                    longDir = (CoordinateComponent.CardinalDirection)readout[5][0];
+                }
+                else
+                {
+                    latitude = CoordinateComponent.DegreeMinutesToDecimal(double.Parse(readout[3]));
+                    latDir = (CoordinateComponent.CardinalDirection)readout[4][0];
+                    longitude = CoordinateComponent.DegreeMinutesToDecimal(double.Parse(readout[5]));
+                    longDir = (CoordinateComponent.CardinalDirection)readout[6][0];
+                }
+                return new Coordinate(latitude, latDir, longitude, longDir);
+            }
+            catch
+            {
+                return GetCoordinates(GPS);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return ToString(CoordinateStyle.DegreeDecimal);
+        }
+        public string ToString(Coordinate.CoordinateStyle style)
+        {
+            return Latitude.ToString(style) + ", " + Longitude.ToString(style);
+        }
+        #endregion
+    }
+
+    public class CoordinateComponent
+    {
+        #region Enums
+        public enum ComponentType
+        { Latitude, Longitude }
+        public enum CardinalDirection
+        { North='N',East='E',South='S',West='W' }
+        #endregion
+        #region Properties
+
+        /// <summary>
+        /// Gets whether the component is latitude or longitude
+        /// </summary>
+        public ComponentType DirectionType
+        {
+            get;
+            private set;
+        }
+
+        public CardinalDirection Direction
+        {
+            get;
+            private set;
+        }
+
+        public double DecimalDegrees
+        {
+            get
+            { return lat; }
+        }
+
+        public int Degrees
+        {
+            get
+            {
+                return (int)Math.Abs(lat);
+            }
+
+        }
+
+        public int Minutes
+        {
+            get
+            {
+                return (int)Math.Abs(lat % 1 * 60);
+            }
+        }
+
+        public double Seconds
+        {
+            get
+            {
+                return Math.Round((Math.Abs(lat % 1) * 60 - Minutes) * 60, 2);
+            }
+        }
+        #endregion
+        #region Variables
+        private double lat;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new CoordinateComponent object with specified degrees, direction and style
+        /// </summary>
+        /// <param name="degrees">Latitude/Longitude in decimal degree form</param>
+        public CoordinateComponent(double degrees, ComponentType type)
+        {
+            lat = degrees;
+            DirectionType = type;
+            if (type == ComponentType.Latitude && Math.Sign(lat) == -1)
+                Direction = CardinalDirection.South;
+            else if (type == ComponentType.Latitude)
+                Direction = CardinalDirection.North;
+            else if (type == ComponentType.Longitude && Math.Sign(lat) == -1)
+                Direction = CardinalDirection.West;
+            else if (type == ComponentType.Longitude)
+                Direction = CardinalDirection.East;
+        }
+
+        public CoordinateComponent(double degrees, CardinalDirection direction)
+        {
+            lat = (direction == CardinalDirection.South || direction == CardinalDirection.West ? -1 : 1) * degrees;
+            Direction = direction;
+            if (direction == CardinalDirection.North || direction == CardinalDirection.South)
+                DirectionType = ComponentType.Latitude;
+            else
+                DirectionType = ComponentType.Longitude;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Converts a coordinate in the form of DDDMM.mmmm to DDD.dddd
+        /// </summary>
+        /// <param name="degreeMinutes">Coordinate in the form of DDDMM.mmmm </param>
+        /// <returns>Coordinate in DDD.dddd form</returns>
+        public static double DegreeMinutesToDecimal(double degreeMinutes)
+        {
+            return (int)degreeMinutes / 100 + Math.Round(degreeMinutes % 100 / 60, 4);
+        }
+      
+        public override string ToString()
+        {
+            return ToString(Coordinate.CoordinateStyle.DegreeDecimal);
+        }
+
+        /// <summary>
+        /// Formats a latitude/longitude in a certain style
+        /// </summary>
+        /// <param name="style">Style to return in</param>
+        /// <returns>String of latitude/longitude in style</returns>
+        public string ToString(Coordinate.CoordinateStyle style)
+        {
+            switch (style)
+            {
+                case Coordinate.CoordinateStyle.DegreeDecimal:
+                    return Math.Abs(DecimalDegrees).ToString() + (char)Direction;
+                case Coordinate.CoordinateStyle.DegreeMinuteDecimal:
+                    return Degrees + "° " + Math.Round((DecimalDegrees % 100), 4) + "\" " + (char)Direction;
+                case Coordinate.CoordinateStyle.DegreeMinuteSecond:
+                    return Degrees + "° " + Minutes + "\" " + Seconds + "\' " + (char)Direction;
+                default:
+                    return Math.Abs(DecimalDegrees).ToString() + (char)Direction;
+            }
+        }
+        #endregion
+    }
+}
---
+++ GPS Scanner/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GPS_Scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            StreamWriter writer = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\GPSLogger-" + DateTime.Now.ToUniversalTime().Date.ToString("yyyy-dd-MM") + ".txt",true);
+            SerialPort GPS = ScanForGPS();
+
+            if (GPS == null)
+            {
+                Console.Write("Please plug in the GPS or close any other program using the port");
+            }
+            else
+            {
+                Console.WriteLine("Now using {0}", GPS.PortName);
+
+                Console.OutputEncoding = GPS.Encoding;
+                List<Tuple<Coordinate, DateTime>> coordinates = new List<Tuple<Coordinate, DateTime>>();
+
+                bool exit = false;
+                DateTime check = DateTime.Now;
+                while (!exit)
+                {
+                    if ((DateTime.Now - check).Seconds >= 5)
+                    {
+                        
+                        Coordinate test = Coordinate.GetCoordinates(GPS);
+                        coordinates.Add(new Tuple<Coordinate, DateTime>(test, DateTime.Now));
+                        writer.Write(coordinates.Last().Item1.Latitude + ":" + coordinates.Last().Item1.Longitude + ":" + coordinates.Last().Item2.ToFileTimeUtc() + ",");
+                        writer.Flush();
+                        check = DateTime.Now;
+                    }
+                    if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Q)
+                    {
+                        exit = true;
+                        writer.Close();
+                    }
+                    if ((DateTime.Now - check).Milliseconds % 1000 <= 10)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Time until next coordinate: {0}s", 5 - (DateTime.Now - check).Seconds);
+                        foreach (Tuple<Coordinate, DateTime> tuple in coordinates)
+                            Console.WriteLine(tuple.Item1.Latitude.DecimalDegrees + ", " + tuple.Item1.Longitude.DecimalDegrees);
+                    }
+                        
+                }
+            }
+        }
+
+        static SerialPort ScanForGPS()
+        {
+            SerialPort GPS = null;
+            //scan and detect GPS
+            foreach (string name in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("Scanning Port: {0}", name);
+                SerialPort test = new SerialPort(name, 4800, Parity.None, 8);
+                test.ReadTimeout = 1000;
+                try
+                {
+                    test.Open();
+                    for (int i = 0; i < 50; i++)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Scanning Port: {0} \n Try: {1}/50", name, i + 1);
+                        if (test.ReadLine().StartsWith("$GP"))
+                        {
+                            GPS = test;
+                            i = 50;
+                            test.Close();
+                        }
+                    }
+                }
+                catch
+                {
+                    test.Close();
+                }
+            }
+            Console.Clear();
+            return GPS;
+        }
+    }
+}
